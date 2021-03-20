b753fb6abaf669dcdcdd40d1dc307e22fcb3a2a5
Greg Folker <greg.folker@microchip.com>
2021-03-20 15:47:05 -0500

    feat: Add example of 'String' type

--- src/main.rs
+++ src/main.rs
@@ -22,4 +22,20 @@ fn main() {
 
     println!("{}", hello_world);
 
+    // The following binds the value '5' to x, and
+    // then makes a copy of x in y
+    let _x = 5;
+    let _y = x;
+
+    // A 'String' is made up of 3 parts:
+    // 1) A pointer to the memory where the string is stored
+    // 2) A length (How much memory, in bytes, the string is currently using)
+    // 3) A capacity (The maximum amount of memory, in bytes, the string can use)
+    //
+    // The above three fields are stored on the stack whereas the
+    // string itself is stored in the heap
+    let s1 = String::from("Hello");
+    let s2 = s1;
+
+
 } // End of the scope
